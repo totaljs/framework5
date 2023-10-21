0b165a9cae121e1bacd4ece78b5fe36ffcff7e12
Peter Širka <petersirka@gmail.com>
2023-10-21 18:25:43 +0200

    Added proxy.

--- routing.js
+++ routing.js
@@ -3,6 +3,12 @@
 // Copyright 2023 (c) Peter Širka <petersirka@gmail.com>
 
 const EMPTYREQSPLIT = ['/'];
+const NEWLINE = '\r\n';
+
+const PROXY_KEEPALIVE = new F.Http.Agent({ keepAlive: true, timeout: 60000 });
+const PROXY_KEEPALIVEHTTPS = new F.Https.Agent({ keepAlive: true, timeout: 60000 });
+const PROXY_OPTIONS = { end: true };
+const PROXY_503HEADERS = { 'cache-control': 'private, no-cache, no-store, max-age=0', 'content-type': 'text/plain; charset=utf-8' };
 
 // Total.js routing
 function Route(url, action, size) {
@@ -109,6 +115,7 @@ function Route(url, action, size) {
 		switch (t.type) {
 			case 'websocket':
 				t.size = F.config.$wsmaxsize;
+				t.connections = [];
 				break;
 			case 'route':
 				t.size = F.config.$httpmaxsize;
@@ -135,15 +142,41 @@ Route.prototype.compare = function(ctrl) {
 	return true;
 };
 
+Route.prototype.remove = function() {
+
+	var self = this;
+	var index = -1;
+
+	switch (self.type) {
+		case 'websocket':
+			index = F.routes.websockets.indexOf(self);
+			if (index !== -1)
+				F.routes.websockets.splice(index);
+			for (let conn of self.connections)
+				conn.destroy();
+			break;
+		case 'file':
+			index = F.routes.files.indexOf(self);
+			if (index !== -1)
+				F.routes.files.splice(index);
+			break;
+		default:
+			index = F.routes.routes.indexOf(self);
+			if (index !== -1)
+				F.routes.routes.splice(index);
+			break;
+	}
+
+};
+
 exports.route = function(url, action, size) {
 	var route = new Route(url, action, size);
 	if (route) {
-		switch (route.method) {
-			case 'SOCKET':
-			case 'WEBSOCKET':
+		switch (route.type) {
+			case 'websocket':
 				F.routes.websockets.push(route);
 				break;
-			case 'FILE':
+			case 'file':
 				F.routes.files.push(route);
 				break;
 			default:
@@ -501,3 +534,243 @@ exports.lookupwebsocket = function(ctrl, auth, skip = false) {
 
 	return routes && routes.length ? compareflags(ctrl, routes, auth) : null;
 };
+
+function Proxy(url, target) {
+
+	var t = this;
+
+	t.url = url.toLowerCase();
+	t.copypath = 'none'; // replace|extend|none
+
+	if ((/^(https|http):\/\//).test(target))
+		t.target = F.Url.parse(target);
+	else
+		t.target = { socketPath: target };
+
+	if (t.target.href) {
+		let index = t.target.href.indexOf('?');
+		if (index !== -1)
+			t.query = t.target.href.substring(index + 1);
+		t.path = t.target.pathname[t.target.pathname.length - 1] === '/' ? t.target.pathname.substring(0, t.target.pathname.length - 1) : t.target.pathname;
+	}
+
+	t.uri = t.target;
+}
+
+Proxy.prototype.copy = function(type) {
+
+	// @type {String} none|replace|extend
+
+	var t = this;
+	if (type === 'replace' && t.target.pathname.length > 1)
+		type = 'extend';
+	t.copypath = type;
+	return t;
+};
+
+Proxy.prototype.after = function(callback) {
+	var t = this;
+	t.$after = callback;
+	return t;
+};
+
+Proxy.prototype.timeout = function(timeout) {
+	var t = this;
+	t.$timeout = timeout ? timeout / 1000 : 0;
+	return t;
+};
+
+Proxy.prototype.check = function(callback) {
+	var t = this;
+	t.$check = callback;
+	return t;
+};
+
+Proxy.prototype.before = function(callback) {
+	var t = this;
+	t.$before = callback;
+	return t;
+};
+
+Proxy.prototype.remove = function() {
+	var t = this;
+	var index = F.routes.proxies.indexOf(t);
+	if (index !== -1)
+		F.routes.proxies.splice(index, 1);
+};
+
+exports.proxy = function(url, target) {
+	var proxy = new Proxy(url, target);
+	F.routes.proxies.push(proxy);
+	return proxy;
+};
+
+exports.lookupproxy = function(ctrl) {
+	for (var proxy of F.routes.proxies) {
+		var u = ctrl.uri.key.substring(0, proxy.url.length);
+		if (u[u.length - 1] !== '/')
+			u += '/';
+		if (u === proxy.url && (!proxy.check || proxy.check(ctrl))) {
+			F.stats.response.proxy++;
+			proxycreate(proxy, ctrl);
+			return true;
+		}
+	}
+};
+
+function proxyheadersws(header, headers) {
+
+	var output = [];
+
+	for (let key in headers) {
+		var value = headers[key];
+		if (value instanceof Array) {
+			for (let item of value)
+				output.push(key + ': ' + item);
+		} else
+			output.push(key + ': ' + value);
+	}
+
+	output.unshift(header);
+	return output.join(NEWLINE) + NEWLINE + NEWLINE;
+}
+
+function proxycreate(proxy, ctrl) {
+
+	var secured = proxy.uri.protocol === 'https:';
+	var uri = {};
+
+	if (proxy.uri.host) {
+		uri.host = proxy.uri.host;
+		uri.hostname = proxy.uri.hostname;
+	} else
+		uri.socketPath = proxy.uri.socketPath;
+
+	var tmp;
+
+	uri.method = ctrl.method;
+	uri.headers = ctrl.headers;
+	ctrl.$proxy = proxy;
+
+	if (uri.socketPath) {
+		uri.path = proxy.copypath == 'none' || proxy.copypath === 'replace' ? ctrl.url.substring(proxy.url.length - 1) : ctrl.uri.pathname;
+	} else {
+
+		if (proxy.copypath === 'none') {
+			uri.path = proxy.uri.path;
+		} else if (proxy.copypath === 'replace')
+			uri.path = ctrl.url.substring(proxy.url.length - 1);
+		else if (proxy.copypath === 'extend') {
+			tmp = ctrl.uri.pathname.substring(proxy.url.length) + (ctrl.uri.search ? ('?' + ctrl.uri.search) : '')
+			uri.path = proxy.path + (tmp ? ((tmp[0] === '/' ? '' : '/') + tmp) : '') + (proxy.query ? (ctrl.uri.search ? ('&' + proxy.query) : ('?' + proxy.query)) : '');
+		} else {
+			tmp = ctrl.uri.pathname + (ctrl.uri.search ? ('?' + ctrl.uri.search) : '')
+			uri.path = proxy.path + (tmp ? ((tmp[0] === '/' ? '' : '/') + tmp) : '') + (proxy.query ? (ctrl.uri.search ? ('&' + proxy.query) : ('?' + proxy.query)) : '');
+		}
+
+		if (proxy.uri.port)
+			uri.port = proxy.uri.port;
+	}
+
+	if (!ctrl.iswebsocket && uri.headers.connection)
+		delete uri.headers.connection;
+
+	uri.headers['x-forwarded-for'] = ctrl.ip;
+	uri.headers['x-forwarded-url'] = ctrl.url;
+	uri.headers['x-forwarded-host'] = ctrl.headers.host;
+
+	uri.agent = secured ? PROXY_KEEPALIVEHTTPS : PROXY_KEEPALIVE;
+
+	delete uri.headers.host;
+
+	proxy.before && proxy.before(uri, ctrl);
+	F.stats.performance.external++;
+	F.stats.request.external++;
+
+	if (ctrl.res.headersSent || ctrl.destroyed)
+		return;
+
+	var get = uri.method === 'GET' || uri.method === 'HEAD' || uri.method === 'OPTIONS';
+	var kind = secured ? F.Https : F.Http;
+	var request = get && !ctrl.iswebsocket ? kind.get(uri, proxycreatecallback) : kind.request(uri, proxycreatecallback);
+
+	request.on('error', proxyerror);
+	request.on('abort', proxyerror);
+	request.on('aborted', proxyerror);
+
+	request.$controller = ctrl;
+	request.$proxy = proxy;
+	request.$timeout = proxy.timeout || F.config.$proxytimeout;
+	request.iswebsocket = !!ctrl.iswebsocket;
+	request.$destroy = proxyerror;
+
+	if (!ctrl.iswebsocket && proxy.timeout) {
+		F.temporary.pending.push(ctrl);
+		F.TUtils.onfinished(ctrl.res, function() {
+			request.$destroyed = true;
+			ctrl.destroyed = true;
+		});
+	}
+
+	if (ctrl.iswebsocket) {
+
+		res.setTimeout(0);
+		res.setNoDelay(true);
+		res.setKeepAlive(true, 0);
+
+		ctrl.iswebsocket && ctrl.head && res.unshift(ctrl.head);
+
+		request.on('response', function (proxyres) {
+
+			if (request.$destroyed)
+				return;
+
+			if (!proxyres.upgrade) {
+				res.write(proxyheadersws('HTTP/' + proxyres.httpVersion + ' ' + proxyres.statusCode + ' ' + proxyres.statusMessage, proxyres.headers));
+				proxyres.pipe(res);
+			}
+
+		});
+
+		request.on('upgrade', function(proxyres, proxysocket, proxyhead) {
+
+			if (request.$destroyed)
+				return;
+
+			if (proxyhead && proxyhead.length)
+				proxysocket.unshift(proxyhead);
+
+			res.write(proxyheadersws('HTTP/1.1 101 Switching Protocols', proxyres.headers));
+			proxysocket.pipe(res).pipe(proxysocket);
+		});
+	}
+
+	if (get)
+		request.end();
+	else
+		ctrl.req.pipe(request, PROXY_OPTIONS);
+}
+
+function proxydestroy(self, err) {
+	if (!self.$destroyed) {
+		self.$destroyed = true;
+		self.$controller.fallback(503, err);
+		self.destroy();
+	}
+}
+
+function proxyerror(err) {
+	proxydestroy(this, err);
+}
+
+function proxycreatecallback(response) {
+	var self = this;
+	if (!self.$destroyed) {
+		let ctrl = self.$controller;
+		ctrl.released = true;
+		ctrl.destroyed = true;
+		ctrl.$proxy.after && self.$proxy.after(response);
+		ctrl.res.writeHead && ctrl.res.writeHead(response.statusCode, response.headers);
+		response.pipe(ctrl.res, PROXY_OPTIONS);
+	}
+}
