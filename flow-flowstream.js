b0221f381d8abd8ef320d0e07d25a7ca56e5f0bb
Peter Širka <petersirka@gmail.com>
2023-11-13 15:47:25 +0100

    Fixed variables.

--- flow-flowstream.js
+++ flow-flowstream.js
@@ -591,6 +591,7 @@ Instance.prototype.reload = function(data, restart = false) {
 		for (let key in data)
 			flow.$schema[key] = data[key];
 		flow.variables = data.variables;
+		flow.variables2 = data.variables2;
 		flow.rewrite(data, () => flow.proxy.refreshmeta());
 	}
 
