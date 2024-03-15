3b5c086a1ae12d15ffece58e015f97a9534b5dfa
Louis Bertson <louisbertson@gmail.com>
2024-03-15 13:44:52 +0000

    Added test for minificators

---
+++ tests/minificators/public/script2.js
@@ -0,0 +1,14 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+/**
+ * This is multiline commets
+ */
+
+// Comment
+arr.wait(function(item) {
+
+    // Another comment
+    console.log(item);
+}, function() {
+    console.log(data);
+});
