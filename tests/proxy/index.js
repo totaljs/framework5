380ead418cc00616fddc7ff60dbc9490d7841c55
Louis Bertson <louisbertson@gmail.com>
2024-03-15 07:43:33 +0000

    Updated test for PROXY

--- tests/proxy/index.js
+++ tests/proxy/index.js
@@ -1,21 +1,34 @@
 /* eslint-disable */
-
 require('../../index');
 require('../../test');
 
 // load web server and test app
-F.http();
+F.run({ release: false, port: 8000 });
+
+var url = 'http://localhost:8000';
+ROUTE('GET /', ($) => $.success());
+PROXY('/cl/', 'https://api.muald.com/cl', false);
 
 ON('ready', function() {
 
 	Test.push('Test Web Proxy', function(next) {
 		// Test.print('String.slug()', [error]);
-		Test.print('String.slug()');
-		next();
+
+		try {
+			RESTBuilder.GET(url + '/cl').exec(function(err, res, output) {
+				console.log(err, res, output);
+				next();
+			});
+		} catch(e) {
+			console.log(e);
+		}
+		
 	});
 
-	Test.run(function() {
-		process.exit(0);
-	});
+	setTimeout(function() {
+		Test.run(function() {
+			process.exit(0);
+		});
+	}, 500);
 
 });
