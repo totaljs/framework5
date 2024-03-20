0bce896257fe6dafd9ba8023f4d193350b1aaa8b
Peter Širka <petersirka@gmail.com>
2024-03-20 08:37:59 +0100

    Improved code.

--- tests/staticfiles/index.js
+++ tests/staticfiles/index.js
@@ -10,46 +10,47 @@ require('../../test');
 
 // load web server and test app
 F.http();
+
 var url = 'http://localhost:8000';
 ON('ready', function() {
 
 	Test.push('Static files -  LOCALIZE', function(next) {
 		var arr = [];
 		var correct = '<h1>Hello world!</h1>';
-		arr.push(function(next_fn) {
+		arr.push(function(resume) {
 			
 			RESTBuilder.GET(url + '/localize.html').exec(function(err, response, output) {
 				Test.print('Localize - default lang', err === null && output.response ===  correct ? null : 'Expected ' + correct);
-				next_fn();
+				resume();
 			});
 		});
 
-		arr.push(function(next_fn) {
+		arr.push(function(resume) {
 			RESTBuilder.GET(url + '/localize.html?lang=en').exec(function(err, response, output) {
 				Test.print('Localize - en lang', err === null && output.response ===  correct ? null : 'Expected ' + correct);
-				next_fn();
+				resume();
 			});
 		});
 
-		arr.push(function(next_fn) {
+		arr.push(function(resume) {
 			correct = '<h1>Ahoj svet!</h1>';
 			RESTBuilder.GET(url + '/localize.html?lang=sk').exec(function(err, response, output) {
 				Test.print('Localize - sk lang', err === null && output.response ===  correct ? null : 'Expected ' + correct);
-				next_fn();
+				resume();
 			});
 		});
 
-		arr.push(function(next_fn) {
+		arr.push(function(resume) {
 			RESTBuilder.GET(url + '/merge1.js').exec(function(err, response, output) {
 				Test.print('Merge - Two files', err === null && output.response !== null ? null : 'Expected ' + correct);
-				next_fn();
+				resume();
 			});
 		});
 
-		arr.push(function(next_fn) {
+		arr.push(function(resume) {
 			RESTBuilder.GET(url + '/merge2.js').exec(function(err, response, output) {
 				Test.print('Merge - With Url', err === null && output.response !== null ? null : 'Expected ' + correct);
-				next_fn();
+				resume();
 			});
 		});
 
@@ -61,9 +62,7 @@ ON('ready', function() {
 		// 	});
 		// });
 
-		arr.async(function() {
-			next();
-		});
+		arr.async(next);
 	});
 
 	setTimeout(function() {
