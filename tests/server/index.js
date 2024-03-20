1c165543fe0a9b224529afd013fe4094c14adb45
Peter Širka <petersirka@gmail.com>
2024-03-20 08:35:59 +0100

    Improved code.

--- tests/server/index.js
+++ tests/server/index.js
@@ -1,30 +1,34 @@
 /* eslint-disable */
 require('../../index');
 require('../../test');
-// unixsocket
-// ip + port
-var opt = {};
+
 Test.push('Test Server', function(next) {
+
 	var arr = [];
+
+	// IP+PORT
 	arr.push(function(next_fn) {
-		opt.ip = '192.168.1.100';
-		opt.port =  5000;
+		var options = {};
+		options.ip = '127.0.0.1';
+		options.port = 8000;
 		Total.load('Workers', function() {
-			var child = NEWTHREAD('~./workers/child', { ip: opt.ip, port: opt.port });
+			var child = NEWTHREAD('child', { ip: options.ip, port: options.port });
 			child.on('message', function() {
-				RESTBuilder.GET('http://{0}:{1}/exit/'.format(opt.ip, opt.port)).exec(function(err, response) {
+				RESTBuilder.GET('http://{0}:{1}/exit/'.format(options.ip, options.port)).exec(function(err, response) {
 					Test.print('Port + Ip: ', err === null && response && response.success === true ? null : 'Expected sucess == true response from child server')
 					next_fn();
 				})
 			});
 		});
 	});
+
+	// Unixsocket
 	arr.push(function(next_fn) {
 		var options = {};
 		options.unixsocket = PATH.root('test.socket');
 		options.unixsocket777 = true;
 		Total.load('Workers', function() {
-			var child2 = NEWTHREAD('~./workers/child2', options);
+			var child2 = NEWTHREAD('child2', options);
 			child2.on('message', function(message) {
 				Test.print('UnixSocket  :', message.unixsocket == options.unixsocket ? null : 'Expected valid Unixsocket');
 				next_fn();
@@ -32,10 +36,10 @@ Test.push('Test Server', function(next) {
 		});
 
 	});
-	arr.async(function() {
-		next();
-	});
+
+	arr.async(next);
 });
+
 Test.run(function() {
 	process.exit(0);
 });
